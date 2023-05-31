16bd0d59fdb0fb329639198a4f57993dc3c2fd93
David.F <119046468+davidFt62@users.noreply.github.com>
2023-05-31 16:13:01 +0200

    initial_commit

---
+++ inc/haut.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <title>Mon Site</title>
+</head>
+<body>
+<header>
+        <div class="conteneur">
+            <nav class="topnav" id="myTopnav"><a href="" title="Mon Site">MonSite.com</a>
+                <a href="<?php echo RACINE_SITE; ?>inscription.php">Inscription</a>
+                <a href="<?php echo RACINE_SITE; ?>connexion.php">Connexion</a>
+                <a href="<?php echo RACINE_SITE; ?>boutique.php">Accès à la boutique</a>
+                <a href="<?php echo RACINE_SITE; ?>panier.php">Voir votre panier</a>
+                <a href="javascript:void(0);" class="icon" onclick="toggleNav()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </nav>
+        </div>
+    </header>
+    <section>
+        <div class="conteneur">
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require('./inc/init.inc.php');
+require('./inc/haut.inc.php');
+
+// Pour le debug
+// var_dump(RACINE_SITE);
+// echo '<br>';
+// echo session_status();
+//notre accueil
+?>
+<h2>Notre page d'accueil</h2>
+<p>Coincée entre le haut et le bas!</p>
+
+
+ <?php
+ require('./inc/bas.inc.php');
